05542d1decba07614d5f356c9139bd0431953e93
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-06-22 17:14:17 -0300

    Conhecendo os DTO

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -7,8 +7,11 @@ import {
   Param,
   Put,
   Post,
+  HttpStatus,
 } from '@nestjs/common';
 import { CoursesService } from './courses.service';
+import { CreateCourseDTO } from './dto/create-course.dto';
+import { UpdateCourseDTO } from './dto/udpate-course.dto';
 
 @Controller('courses')
 export class CoursesController {
@@ -25,16 +28,16 @@ export class CoursesController {
   }
 
   @Post()
-  create(@Body() body) {
-    return this.courseService.create(body);
+  create(@Body() createCourseDTO: CreateCourseDTO) {
+    return this.courseService.create(createCourseDTO);
   }
 
   @Put(':id')
-  update(@Param('id') id: number, @Body() body) {
-    return this.courseService.update(+id, body);
+  update(@Param('id') id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
+    return this.courseService.update(+id, updateCourseDTO);
   }
 
-  @HttpCode(204)
+  @HttpCode(HttpStatus.NO_CONTENT)
   @Delete(':id')
   remove(@Param('id') id: number) {
     return this.courseService.remove(+id);
