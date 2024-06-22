295c10f79a3d3d14f722afbc132633336982dbb0
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-06-22 16:38:45 -0300

    Trabalhando com Services no Nest - 2

--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -11,4 +11,33 @@ export class CoursesService {
       tags: ['node.js', 'nest.js', 'javascript', 'typescript'],
     },
   ];
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: number) {
+    return this.courses.find((course) => course.id === id);
+  }
+
+  create(createCourseDTO: any) {
+    this.courses.push(createCourseDTO);
+  }
+
+  update(id: number, updateCourseDTO: any) {
+    const existingCourse = this.findOne(id);
+    if (existingCourse) {
+      const index = this.courses.findIndex((course) => course.id === id);
+      this.courses[index] = {
+        id,
+        ...updateCourseDTO,
+      };
+    }
+  }
+
+  remove(id: number) {
+    const index = this.courses.findIndex((course) => course.id === id);
+    if (index >= 0) {
+      this.courses.splice(index, 1);
+    }
+  }
 }
