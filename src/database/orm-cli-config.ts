7bfcebc3e785578ce521daba26bcb8aa7c7ce58d
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-06-24 12:56:09 -0300

    Criando a tabela tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,9 +1,10 @@
 import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1719243028886 } from 'src/migrations/1719243028886-CreateCoursesTable';
+import { CreateTagsTable1719244010506 } from 'src/migrations/1719244010506-CreateTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1719243028886],
+  migrations: [CreateCoursesTable1719243028886, CreateTagsTable1719244010506],
 });
---
+++ src/migrations/1719244010506-CreateTagsTable.ts
@@ -0,0 +1,32 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTagsTable1719244010506 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
+    await queryRunner.createTable(
+      new Table({
+        name: 'tags',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'CURRENT_TIMESTAMP',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tags');
+  }
+}
