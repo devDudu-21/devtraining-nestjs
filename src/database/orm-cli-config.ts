e0da55c0148e518ee9a1017d8c61fbf5a2b2de56
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-06-24 13:15:22 -0300

    Criando a tabela courses_tags através de migração

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -2,9 +2,14 @@ import { DataSource } from 'typeorm';
 import { dataSourceOptions } from './database.module';
 import { CreateCoursesTable1719243028886 } from 'src/migrations/1719243028886-CreateCoursesTable';
 import { CreateTagsTable1719244010506 } from 'src/migrations/1719244010506-CreateTagsTable';
+import { CreateCoursesTagsTable1719245308930 } from 'src/migrations/1719245308930-CreateCoursesTagsTable';
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
   synchronize: false,
-  migrations: [CreateCoursesTable1719243028886, CreateTagsTable1719244010506],
+  migrations: [
+    CreateCoursesTable1719243028886,
+    CreateTagsTable1719244010506,
+    CreateCoursesTagsTable1719245308930,
+  ],
 });
