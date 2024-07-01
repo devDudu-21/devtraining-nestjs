21f8f9769384911f129e1a2806f2017f8cfae534
Eduardo Fernandes <edufernandes.1895@gmail.com>
2024-07-01 17:12:03 -0300

    Trabalhando com variáveis de ambiente no NestJs

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,26 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from 'src/courses/entities/courses.entity';
 import { Tag } from 'src/courses/entities/tags.entity';
-import { DataSourceOptions } from 'typeorm';
-
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'docker',
-  database: 'devtraining',
-  entities: [Course, Tag],
-  synchronize: false,
-};
 
 @Module({
   imports: [
     TypeOrmModule.forRootAsync({
-      useFactory: async () => {
-        return { ...dataSourceOptions };
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: Number(configService.get('DB_PORT')),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASS'),
+          database: configService.get('DB_NAME'),
+          entities: [Course, Tag],
+          synchronize: false,
+        };
       },
+      inject: [ConfigService],
     }),
   ],
 })
--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1719243028886 } from 'src/migrations/1719243028886-CreateCoursesTable';
 import { CreateTagsTable1719244010506 } from 'src/migrations/1719244010506-CreateTagsTable';
 import { CreateCoursesTagsTable1719245308930 } from 'src/migrations/1719245308930-CreateCoursesTagsTable';
 import { AddCoursesIdToCoursesTagsTable1719245817961 } from 'src/migrations/1719245817961-AddCoursesIdToCoursesTagsTable';
 import { AddTagsIdToCoursesTagsTable1719252036211 } from 'src/migrations/1719252036211-AddTagsIdToCoursesTagsTable';
+import { Course } from 'src/courses/entities/courses.entity';
+import { Tag } from 'src/courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
